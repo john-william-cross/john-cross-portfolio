0ea38ad110c728da2ecef1d5696a74ff3d91bc12
John Cross <john.william.cross@gmail.com>
2020-10-13 15:46:27 -0700

    finish components and props video 206A

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import "./style/master.scss";
+import "./components/Project";
+import Project from "./components/Project";
+import { projects } from "./data/projects";
+
+console.log(projects);
 
 function App() {
    return (
       <>
-         <h1 className="text-primary">Hello world</h1>
+         <h1 className="text-danger">Hello world</h1>
+         {/* This is a comment in JSX */}
+         {projects.map((project) => {
+            return <Project project={project} key={project.id} />; // TODO: add key
+         })}
       </>
    );
 }
---
+++ src/components/Project.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Project(props) {
+   console.log(props);
+   return (
+      <>
+         <h4 className="text-primary">
+            {props.project.title} ({props.project.rating})
+         </h4>
+      </>
+   );
+}
+
+export default Project;
