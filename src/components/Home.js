d53fda046e0b89246b2c7b8a31407bf83b2cca16
John Cross <john.william.cross@gmail.com>
2020-10-14 14:07:09 -0700

    create Bio.js component

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,8 @@ import "../style/master.scss";
 import "./Project";
 import Project from "./Project";
 import { projects } from "../data/projects";
-import githubLogo from "../icon/github.svg";
-import linkedInLogo from "../icon/linkedin.svg";
-import youtubeLogo from "../icon/youtube.svg";
+
+import Bio from "./Bio";
 
 console.log(projects);
 
@@ -14,54 +13,7 @@ export default function Home() {
       <div className="container">
          <div className="row mt-2">
             <div className="col-12 col-lg-10 offset-lg-1 col-xl-8 offset-xl-2">
-               <h1 className="display-3 mb-1 text-center title">John Cross</h1>
-               <p className="lead font-weight-normal mb-4 text-center">
-                  Full stack developer
-               </p>
-               <p className="lead font-weight-normal text-center mb-4 mb-sm-5">
-                  <a
-                     href="https://github.com/john-william-cross"
-                     target="_blank"
-                     rel="noopener noreferrer"
-                     className="d-inline-block"
-                  >
-                     <img
-                        src={githubLogo}
-                        width="24px"
-                        className="mt-n1"
-                        alt="Github logo"
-                     />
-                     <span className="ml-2">Github</span>
-                  </a>
-                  <a
-                     href="https://www.linkedin.com/in/john-william-cross/"
-                     target="_blank"
-                     rel="noopener noreferrer"
-                     className="d-inline-block"
-                  >
-                     <img
-                        src={linkedInLogo}
-                        width="20px"
-                        className="ml-4 mt-n1"
-                        alt="LinkedIn logo"
-                     />
-                     <span className="ml-2">LinkedIn</span>
-                  </a>
-                  <a
-                     href="https://www.youtube.com/channel/UCXJsMHVQxBvbhN98CqGdG9Q"
-                     target="_blank"
-                     rel="noopener noreferrer"
-                     className="d-inline-block"
-                  >
-                     <img
-                        src={youtubeLogo}
-                        width="28px"
-                        className="ml-4 mt-1"
-                        alt="Youtube logo"
-                     />
-                     <span className="ml-2">YouTube</span>
-                  </a>
-               </p>
+               <Bio />
                <div className="row mb-2">
                   <div className="col-12 col-sm-8">
                      <input
